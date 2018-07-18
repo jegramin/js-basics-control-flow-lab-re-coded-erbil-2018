26b0970d18ebaceeb07c49cb2f79002fdee1ba55
Mohammed Ameen <jegroamin@gmail.com>
2018-07-18 21:20:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function scuberGreetingForFeet(ride){
   if(ride<=400){
     return 'This one is on me!'
-  }
-  else if(ride>2000){
+    if(ride>2001){
       return 'I will gladly take your thirty bucks.'
     }
+  }
 }
